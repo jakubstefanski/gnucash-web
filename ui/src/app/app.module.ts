a1b125f9d354cb74695b240c864399f97e855284
Jakub Stefanski <js@jakubstefanski.com>
2021-01-05 01:08:22 +0100

    Download generated CSV export file

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatTableModule } from "@angular/material/table";
 import { AppRoutingModule } from "@src/app/app-routing.module";
 import { AppComponent } from "@src/app/app.component";
 import { BookComponent } from "@src/app/book/book.component";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { BookComponent } from "@src/app/book/book.component";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ui/src/app/book/book.component.ts
+++ ui/src/app/book/book.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 
+import { saveAs } from "file-saver";
+
 import { formatCommodityQuantity, isTransaction, Split, Transaction } from "@src/app/app.domain";
 import { isNegative } from "@src/app/decimal";
+import { ExportService } from "@src/app/export/export.service";
 import { TRANSACTIONS } from "@src/app/fixtures";
 
 @Component({
@@ -11,13 +14,24 @@ import { TRANSACTIONS } from "@src/app/fixtures";
 })
 export class BookComponent implements OnInit {
   displayedColumns: string[] = ["date", "num", "description", "account", "fundsIn", "fundsOut"];
-  dataSource = TRANSACTIONS
-    .sort((a, b) => a.date.getTime() - b.date.getTime())
-    .flatMap(tx => [tx, ...tx.splits]);
+  transactions: Transaction[] = TRANSACTIONS;
 
-  constructor() { }
+  get dataSource() {
+    return this.transactions
+      .sort((a, b) => a.date.getTime() - b.date.getTime())
+      .flatMap(tx => [tx, ...tx.splits]);
+  }
+
+  constructor(private exportService: ExportService) { }
 
   ngOnInit(): void {
+  }
+
+  exportToCsv() {
+    const csv = this.exportService.exportToCsv(this.transactions);
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
+    const date = new Date().toISOString().substr(0, 10);
+    saveAs(blob, `gnucash-export-${date}.csv`);
   }
 
   formatDate(row: Transaction | Split): string {
